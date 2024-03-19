113219ff64a0a8a1dbe3d4a3b53b7cc2cceb3285
GYPpro <gypplus@outlook.com>
2024-03-19 20:28:20 +0800

    sync

---
+++ WEEK1/week1.h
@@ -0,0 +1,13 @@
+#include <math>
+#include <algorithm>
+
+using namespace lineSearch{
+	double find_minnum(
+			/*function*/ func,
+			double l,//left range
+			double r,//right range
+			double acc
+		)
+	{
+	}
+}
