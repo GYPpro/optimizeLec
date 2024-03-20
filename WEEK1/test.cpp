e0cf0c5d0e09a5161650c56f754412d6920bbd99
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2024-03-20 10:13:00 +0800

    sycn

---
+++ WEEK1/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int f(int a)
+{
+
+}
+int main()
+{
+    auto x = f;
+}
--- WEEK1/week1.h
+++ WEEK1/week1.h
@@ -1,13 +1,50 @@
-#include <math>
+#include <math.h>
 #include <algorithm>
 
-using namespace lineSearch{
+namespace lineSearch{
+
+	const int _BINARY_ = 1;		  //binary search method
+	const int _GOLDEN_RATIO_ = 2; //0.618 method
+	const int _FIBONACCI_ = 3;	  //Fibonacci method
+
 	double find_minnum(
-			/*function*/ func,
-			double l,//left range
-			double r,//right range
-			double acc
+			double (*func)(double x),// inputed unimodal function
+			double l,				 // left range
+			double r,				 // right range
+			double acc,				 // Search accuracy
+			int mod					 // Search mod
 		)
+	/**
+	 * @attention 
+	 * function will return -1 and throw exceptions while getting illegal input
+	 * given a illegal input function is undefined behavior
+	 * @brief
+	 * Finding the minnum num of the input unimodal function at a given accuracy
+	 */
 	{
+		if (l > r) {throw "Illegal Range Execption";return -1;}
+		// if (l - r > acc) {throw "Too sm"}
+		switch (mod)
+		{
+		case _BINARY_:
+		{
+			double idx = l;
+			
+		} break;
+		case _GOLDEN_RATIO_:
+		{
+
+		} break;
+		case _FIBONACCI_:
+		{
+
+		} break;
+		
+		default:
+		{
+			throw "Unexpection Search Mod Exception";
+		}
+			break;
+		}
 	}
 }
